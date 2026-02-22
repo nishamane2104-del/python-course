282a7880894cfd63db6d0490642fadaef7ed91e2
Nisha Mane <nishamane2104@gmail.com>
2026-02-22 16:14:02 +0530

    meaningful messages

--- Introduction.py
+++ Introduction.py
@@ -1,3 +1,6 @@
+from multiprocessing import Value
+
+
 print("hello Aniket Shinde")
 print("Welcome to Python Programming")
 print("hello Aniket Shinde") 
@@ -127,3 +130,29 @@ print("Data type of tuple1 =", type(tuple1))
 str_1 = "Hello, World!"
 print("Initialized String:", str_1)
 print("Data type of str_1 =", type(str_1))
+# range
+r = range(3, 26, 6)
+print("Range:", r)
+print("Range as list:", list(r))
+print('Data type:', type(r)) 
+# set 
+set1 = {"mango", "banana",1.3, True}
+set2 = set([1, 0.5, "hello",1.7e2, True])
+print("Set 1:", set1)
+print("Data type of set 1:", type(set1))
+print("Set 2:", set2)
+print("Data type of set 2:", type(set2))
+list1 = ["berries", "orange", "grapes", "apple", "banana"]
+f1 = frozenset(list1)
+print("Frozenset:", f1)
+print("Data type of frozenset:", type(f1))
+person1 = {"name" : "Nisha", "age" : 25, "city" : "Pune"}
+print("Person Details:\n", person1)
+print("Data type of person1:", type(person1))
+# boolean
+a1= True
+a2= False
+print("Value of a1:", a1)
+print("Data type of a1 =", type(a1))
+print("Value of a2:", a2)
+print("Data type of a2 =", type(a2))
