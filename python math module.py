8ed873c045ed834e4fddd710ca394d34c5a9af8b
Nisha Mane <nishamane2104@gmail.com>
2026-02-25 20:39:51 +0530

    math module

---
+++ python math module.py
@@ -0,0 +1,8 @@
+# importing math module
+import math
+print("Value of Pi=", math.pi) #pi
+print("cos(pi) =", math.cos(math.pi)) #cosine of pi
+print("e^5 =", math.exp(5)) #exponential of 5
+print("log10(10000) =", math.log10(10000)) #logarithm of 10000 to the base 10
+print("sinh(1/2) =", math.sinh(1/2))     #hyperbolic sine of 1/2
+print("7! =", math.factorial(7)) #factorial of 7
